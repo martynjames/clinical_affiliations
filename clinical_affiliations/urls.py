a80e80dfa075b39c900c4b211900405ab9d46206
Martyn James <mjames@edx.org>
2015-02-26 23:35:51 -0500

    logout

--- clinical_affiliations/urls.py
+++ clinical_affiliations/urls.py
@@ -10,6 +10,7 @@ urlpatterns = patterns(
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'admin/login.html'}),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
     url(r'^$', 'communications.views.institutions', name='institutions'),
     url(r'^communications/', include('communications.urls'), name='communications'),
 )
